3bc8bbbe2d3be48085c7f23943e06883a77de5f9
clauber-DEV <clauber.porto@hotmail.com>
2023-07-21 09:19:45 -0300

    projeto em HTML
    proj 01

---
+++ proj-001/ficha.js
@@ -0,0 +1,8 @@
+function dados(){
+    let nome = document.querySelector('input#name')
+    let ano = documet.querySelector('input#ano')
+    let sal = document.querySelector('input#salario')
+    if(ano.value.length == 0 ){
+        window.alert('vazio')
+    }
+}
