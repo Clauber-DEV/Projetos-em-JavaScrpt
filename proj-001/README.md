25450346760ed87a6c4dd71366870b9ab030f00c
clauber-DEV <clauber.porto@hotmail.com>
2023-07-24 16:05:57 -0300

    Projeto v0.1
    Cadastro básico de funcionários

---
+++ proj-001/README.md
@@ -0,0 +1,4 @@
+# Ficha de Cadastro versão 0.1
+ficha básica de cadastro de funcionário utilizando comando de condições para vlidação de dados
+pedindo is seguintes quesitos:
+nome, ano de nascimanto e salário.
--- proj-001/ficha.js
+++ proj-001/ficha.js
@@ -1,8 +1,17 @@
-function dados(){
-    let nome = document.querySelector('input#name')
-    let ano = documet.querySelector('input#ano')
+function dados() {
+    let nome = document.querySelector('input#nome')
+    let ano = document.querySelector('input#ano')
     let sal = document.querySelector('input#salario')
-    if(ano.value.length == 0 ){
-        window.alert('vazio')
+    let ficha = document.querySelector('p#ficha')
+
+    if (nome.value.length == '' || ano.value.length == 0 || sal.value.length == 0) {
+        window.alert('Favor preencher todos os campos antes de salvar!')
+    } else {
+        let n = String(nome.value)
+        let a = Number(ano.value)
+        let s = Number(sal.value)
+        ficha.innerHTML = `Ficha Funcional <br> Nome:${n} <br> Ano de Nascimento:${a} <br> Salário:${s}`
+
+
     }
 }
