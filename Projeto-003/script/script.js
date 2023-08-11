b838bb2928e24f107f2bc98f0c8d0f6c11084cb9
clauber-DEV <clauber.porto@hotmail.com>
2023-08-11 08:39:16 -0300

    projeto 003
    Conversor em metros

--- Projeto-003/script/script.js
+++ Projeto-003/script/script.js
@@ -1,9 +1,24 @@
-function converter(){
-    let numero = document.querySelector(`input#numero`)
-    let resultado = document.querySelector(`input#resultado`)
-    if(numero.value.length == 0 ){
-        window.alert = (`vazio`)
-    }else{
-        window.alert = (`cheio`)
+function converter() {
+    let metros = document.querySelector(`input#metros`)
+    let res = document.querySelector(`p#resultado`)
+    if (metros.value.length == 0) {
+        window.alert(`Insira um número para converter!`)
+    } else {
+        let mt = Number(metros.value)
+
+        let m1 = mt / 1000
+        let m2 = mt / 100
+        let m3 = mt / 10
+        let m4 = mt * 10
+        let m5 = mt * 100
+        let m6 = mt * 1000
+
+        res.innerHTML = `Converção:<br>
+        ${m1} Km => Kilometro<br> 
+        ${m2} Hm => Hequitometro<br> 
+        ${m3} Dam => Decametro<br> 
+        ${m4} dm => decimetro<br> 
+        ${m5} cm => centimetro<br> 
+        ${m6} mm => milimetro<br>`
     }
 }
