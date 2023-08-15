e3f65ff53fd12f3a36972440d75ee01ff54c9fb0
clauber-DEV <clauber.porto@hotmail.com>
2023-08-15 10:46:55 -0300

    Update script.js

--- projeto-004/script/script.js
+++ projeto-004/script/script.js
@@ -1,23 +1,24 @@
-const preco = document.querySelector(`input#preco`);
-const resultado = document.querySelector(`div#resultado`);
+const preco = document.querySelector('#preco');
+const resultado = document.querySelector('#resultado');
 
 function calcularPeriodo(percentual) {
     if (preco.value.length === 0) {
-        window.alert(`Por favor, digite o preço antes de clicar no período!`);
-    } else {
-        const preco1 = Number(preco.value);
-        const preco2 = preco1 + (percentual * preco1 / 100);
-
-        let descricao = "";
-        if (percentual > 0) {
-            descricao = `terá mais ${percentual}% no valor`;
-        } else {
-            descricao = `terá menos ${Math.abs(percentual)}% no valor`;
-        }
-
-        resultado.innerHTML = `Nesse período seu produto ${descricao}.<br>
-            De R$${preco1.toFixed(2)} passará para R$${preco2.toFixed(2)}.`;
+        window.alert('Por favor, digite o preço antes de clicar no período!');
+        return;
     }
+
+    const preco1 = Number(preco.value);
+    const preco2 = preco1 + (percentual * preco1 / 100);
+
+    let descricao = "";
+    if (percentual > 0) {
+        descricao = `terá mais ${percentual}% no valor`;
+    } else {
+        descricao = `terá menos ${Math.abs(percentual)}% no valor`;
+    }
+
+    resultado.innerHTML = `Nesse período seu produto ${descricao}.<br>
+        De R$${preco1.toFixed(2)} passará para R$${preco2.toFixed(2)}.`;
 }
 
 function carnaval() {
