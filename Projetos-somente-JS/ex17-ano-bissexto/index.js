731e7802f4368c60efb6b5ca378e5f9c73bac679
clauber-DEV <clauber.porto@hotmail.com>
2023-11-04 11:05:16 -0300

    Qual ano é bissexto?

---
+++ Projetos-somente-JS/ex17-ano-bissexto/index.js
@@ -0,0 +1,23 @@
+/** O programa deve receber um ano qualquer do usuário e
+ *  avaliar se é bissexto ou nao utilizando condições de avaliação boleanas.*/
+
+console.log("Quer saber qual ano é bissexto?")
+
+const readline = require(`readline`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Então digite ele aqui: ", function (anoQualquer) {
+
+    let ano = parseInt(anoQualquer)
+
+    if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) {
+        console.log(`O ano ${ano} é bissexto.`)
+    } else {
+        console.log(`O ano ${ano} não é bissexto.`)
+    }
+    rl.close()
+})
