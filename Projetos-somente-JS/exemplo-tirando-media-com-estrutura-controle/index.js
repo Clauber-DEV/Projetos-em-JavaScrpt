599a32cc1c3c6b5e6edd55d0472d3ecfb16e8363
clauber-DEV <clauber.porto@hotmail.com>
2023-11-13 09:28:40 -0300

    function async e while

--- Projetos-somente-JS/exemplo-tirando-media-com-estrutura-controle/index.js
+++ Projetos-somente-JS/exemplo-tirando-media-com-estrutura-controle/index.js
@@ -1,18 +1,18 @@
-const readline = require('readline'); // modulo para entrada de dados
+const readline = require('readline'); // Modulo para entrada de dados.
 
 const rl = readline.createInterface({
-    input: process.stdin, // entrada padrão
-    output: process.stdout // saída padrão
+    input: process.stdin, // Entrada padrão.
+    output: process.stdout // Saída padrão.
 });
 
-async function lerNotas(total) { // função assíncrona "async"
-    let somaNotas = 0;
-    let c = 1;
+async function lerNotas(total) { // Função assíncrona "async" que espera uma promessa.
+    let somaNotas = 0; // O totalizador deve ser declarado fora da condição.
+    let c = 1; // Contador dos ciclos.
 
-    while (c <= total) { // estrutura de controle "while" == enquanto
-        const resposta = await pergunta("Digite suas notas: ");  // função com comando "await" == aguarde irá pausar a execução do programa até que o usuário forneça as informações.
+    while (c <= total) { // Estrutura de controle "while" == enquanto.
+        const resposta = await pergunta("Digite suas notas: ");  // Função com comando "await" == aguarde irá pausar a execução do programa até que o usuário forneça as informações.
 
-        let notas = parseFloat(resposta); // convesor dos dados da função resposta
+        let notas = parseFloat(resposta); // Convesor dos dados da função resposta.
         somaNotas += notas; // somaNotas no caso é um acumulador.
         console.log(`Ciclo ${c} nota ${notas}`);
         console.log(`Ciclo ${c} soma ${somaNotas}`);
@@ -27,11 +27,11 @@ async function lerNotas(total) { // função assíncrona "async"
     }
 }
 
-function pergunta(pergunta) { // função aguardando retorno de uma promessa "promise"
-    return new Promise((resolve) => { // promessa com parametro resolve. Esse resolve irá receber os dados fornecidos pela rl.question
-        rl.question(pergunta, resolve); // tudo que será entregue ao parametro "resolve" etá sendo tratado nas linhas 13 a 26.
+function pergunta(pergunta) { // Função aguardando retorno de uma promessa "promise".
+    return new Promise((resolve) => { // Promessa com parametro resolve. Esse resolve irá receber os dados fornecidos pela rl.question.
+        rl.question(pergunta, resolve); // Tudo que será entregue ao parametro "resolve" está sendo tratado nas linhas 13 a 26.
     });
-}
+};
 
 rl.question("Quantos números deseja?: ", function (total) {
     let tot = parseInt(total);
