b8862a35a514596e74d812cf947383bea7b1c452
clauber-DEV <clauber.porto@hotmail.com>
2023-11-30 15:10:32 -0300

    números primos

---
+++ Projetos-somente-JS/ex48-repeticao-com-lacos-de-repeticao/index.js
@@ -0,0 +1,39 @@
+/* M03 ex 048 repetição com variaveis de controle
+ * O programa deve receber um valor inteiro do usuário 
+ * informar se o valor implemetado é um número primo
+ * Autor: Clauber Cardoso Porto
+ */
+
+const readline = require('readline');
+const entrada = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function numeroprimo() {
+    let ciclo = 0, numero = 0, div = 0
+
+    entrada.question("Número: ", function (num) {
+        numero = parseInt(num)
+
+        for (ciclo = 1; ciclo <= numero; ciclo++) {
+
+            if (numero % ciclo === 0 || numero / ciclo === numero) {
+                console.log("[", ciclo, "]")
+                div = div + 1
+            } else {
+                console.log(" ", ciclo, " ")
+            }
+
+        }
+        entrada.close()
+        if (div === 2) {
+            console.log("O número ", numero, " foi divisivel 2 vezes")
+            console.log("Esse é um número PRIMO")
+        } else {
+            console.log("O número ", numero, " não é PRIMO.")
+        }
+    })
+}
+
+numeroprimo();
